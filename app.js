b3f8c852661b2b1fa902d518186328dec5d99816
Aadish Jain <theaadishjain@gmail.com>
2025-12-26 18:43:12 +0530

    initial and database setup

---
+++ app.js
@@ -0,0 +1,39 @@
+const express= require("express");
+const app=express();
+const mongoose= require("mongoose");
+const Listing= require("./models/listing.js");
+
+const MONGO_URL= "mongodb://127.0.0.1:27017/StayEase";
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req,res)=>{
+    res.send("Hi,I am root");
+});
+
+app.get("/testListing",async(req,res)=>{
+    let sampleListing= new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute,Goa",
+        country: "India",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfull testing"); 
+});
+
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+});
